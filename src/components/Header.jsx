9f4ef2b1dac1b1f96552f60afaf770265507e854
Muruganandan (Raja) <msingaram@ebsco.com>
2019-03-31 00:09:18 -0400

    Clean-up addition of Bootswatch theme

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container">
+        <span class="navbar-brand mr-auto mr-lg-2">T.R.App</span>
+      </div>
+    </div>
+  );
+}
